b56204d4c37acf8ca10f518b1a67cd5309823f03
MOHAMED BAKULY <119895383+MOHAMEDBAKULY@users.noreply.github.com>
2024-01-23 15:16:52 +0300

    Changed All APIs to Try....Catch Method

--- AsyncAwait....ErrorHandling/script.js
+++ AsyncAwait....ErrorHandling/script.js
@@ -1,6 +1,33 @@
 const getJokes = async () => {
-    try {
+    try{
+        const res = await fetch('https://api.chucknorris.io/jokes/random')
+        // const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=20')
+
+        // const res = await fetch('http://httpstat.us/416')
+
+        const data = await res.json();
+
+        if(res.status === 404){
+            if(res.status === 416){
+               throw new Error('Requested Range Not Satisfiable')
+            }
+            throw new Error('There is a problem on our side, wait we are fixing it')
+        }
+
+        console.log(data)
+    }catch(error)  {
+        console.log(error)
+    } 
+  
+};
+
+getJokes()
+
+
+const getPosts = async () => {
+
         // const res = await fetch('https://api.chucknorris.io/jokes/rand0m')
+        // const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=20')
 
         const res = await fetch('http://httpstat.us/416')
 
@@ -14,27 +41,35 @@ const getJokes = async () => {
         }
 
         console.log(data)
-        
-    } catch (error) {
-        console.log(error)
-    }
+
+  
 };
 
-getJokes()
+getPosts().catch((error) => console.log(error));
 
-const url = 'https://sales-tax-rates1.p.rapidapi.com/v/api/?zip=99502';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-		'X-RapidAPI-Host': 'sales-tax-rates1.p.rapidapi.com'
-	}
-};
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+
+
+
+
+
+
+
+
+
+// const url = 'https://sales-tax-rates1.p.rapidapi.com/v/api/?zip=99502';
+// const options = {
+	// method: 'GET',
+	// headers: {
+		// 'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+		// 'X-RapidAPI-Host': 'sales-tax-rates1.p.rapidapi.com'
+	// }
+// };
+// 
+// try {
+	// const response = await fetch(url, options);
+	// const result = await response.text();
+	// console.log(result);
+// } catch (error) {
+	// console.error(error);
+// }
--- PromiseAll/script.js
+++ PromiseAll/script.js
@@ -20,20 +20,74 @@ const getData = (endpoint) => {
     })
   }
   
-const moviePromise = getData('./movies.json')
-const actorsPromise = getData('./actors.json')
-const directorsPromise = getData('./directors.json')
+// const moviePromise = getData('./movies.json')
+// const actorsPromise = getData('./actors.json')
+// const directorsPromise = getData('./directors.json')
 
-Promise.all([moviePromise, actorsPromise, directorsPromise])
- .then((data) => {
-  console.log(data)
- })
- .catch((error) => {
-  console.log(error)
- })
+// Promise.all([moviePromise, actorsPromise, directorsPromise])
+//  .then((data) => {
+//   console.log(data)
+//  })
+//  .catch((error) => {
+//   console.log(error)
+//  })
 
 
 
+async function getAllData () {
+   const directors = await getData('./directors.json')
+   const movies = await getData('./movies.json')
+   const actors = await getData('./actors.json')
+   console.log(actors, directors, movies)
+
+}
+
+const getAllDataWithFecth = async ()  => {
+     const moviesRes = await fetch('./movies.json')
+     const movies = await moviesRes.json();
+
+     const directorsRes = await fetch('./directors.json')
+     const directors = await directorsRes.json();
+
+     const actorsRes = await fetch('./actors.json')
+     const actors = await actorsRes.json();
+
+     console.log(movies, directors, actors)
+}
+
+
+
+const getAllDataWithPromiseAll = async ()  => {
+     const [moviesRes, directorsRes, actorsRes] = await Promise.all([
+      fetch('./movies.json'),
+      fetch('./directors.json'),
+      fetch('./actors.json')
+     ])
+
+     const movies = await moviesRes.json();
+     const directors = await directorsRes.json();
+     const actors = await actorsRes.json();
+
+     console.log(movies, directors, actors)
+
+
+}
+
+
+const getAllDataWithPromiseAll2 = async ()  => {
+  const [movies, directors, actors] = await Promise.all([
+   fetch('./movies.json').then(res => res.json()),
+   fetch('./directors.json').then(res => res.json()),
+   fetch('./actors.json').then(res => res.json())
+  ]);
+ 
+
+  console.log(movies, directors, actors);
+}
+// getAllData();
+// getAllDataWithFecth();
+// getAllDataWithPromiseAll();
+getAllDataWithPromiseAll2();
 
 
 
